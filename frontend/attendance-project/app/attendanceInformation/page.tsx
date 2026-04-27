ea19f92ebd8fd608e93fab890a2b42f9256d514c
kosuke <jobsuke05@gmail.com>
2026-04-27 10:48:42 +0900

    勤怠情報ページの実装

---
+++ frontend/attendance-project/app/attendanceInformation/page.tsx
@@ -0,0 +1,9 @@
+import { AttendanceInformationLogicComponent } from "@/component/attendanceInformation/attendanceInformationLogicComponent";
+
+// 勤怠情報ページ
+export default function AttendanceInformationPage() {
+
+  return (
+    <AttendanceInformationLogicComponent />
+  )
+};
