0fe8cf14d6b59dbc6b57b8a6fdbe8f8d224eb256
kosuke <jobsuke05@gmail.com>
2026-06-25 11:10:46 +0900

    ユーザーのログイン処理の実装

--- backend/src/services/user/login/userLoginService.ts
+++ backend/src/services/user/login/userLoginService.ts
@@ -1,6 +1,21 @@
+import type { QueryResult } from "pg";
 import type { UserLoginType } from "../../../../types/user/userType.js";
 import { UserLoginRepository } from "../../../repository/user/login/userLoginRepository.js";
+import bcrypt from "bcrypt";
 
+/**
+ * ・メールアドレス・パスワードの両方が合致するカラムを取得
+ * ①serviceからrepositoryに対して、メールアドレスのみを渡す
+ * ②メールアドレスに合致するカラムを取得
+ * ③service内で仮引数で受け取ったパスワードが合致するカラムを探す
+ */
 export const UserLoginService = async (data: UserLoginType) => {
-  await UserLoginRepository();
+  const user: QueryResult = await UserLoginRepository(data.mailAddress);
+
+  if(!user) {
+    throw new Error("メールアドレスかパスワードが違います。");
+  }
+
+  // パスワードの照合
+  const isMatch = await bcrypt.compare(data.password, user.pass_word);
 };
